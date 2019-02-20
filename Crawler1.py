1f57d1a4f1ee76fd8bc815e15535b456322515c7
Zengqiu <myzqyc1@gmail.com>
2019-02-20 17:59:19 +0800

    download image

--- Crawler1.py
+++ Crawler1.py
@@ -19,9 +19,18 @@ def getHTMLText(url):
     except:
         return "出现异常"
 
-if __name__ == "__main__":
-    url = "http://www.baidu.com/s"
-    kv = {'wd': 'Python'}
-    r = requests.get(url, params=kv)
-    r.encoding = r.apparent_encoding
-    print(r.text[:1000])
+
+# if __name__ == "__main__":
+    # url = "http://www.baidu.com/s"
+    # kv = {'wd': 'Python'}
+    # r = requests.get(url, params=kv)
+    # r.encoding = r.apparent_encoding
+    # print(r.text[:1000])
+    url = "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2184669578,\
+        2405818199&fm=58&w=121&h=140&img.JPEG"
+    path = ".\UCAS.jpg"
+    r = requests.get(url)
+    print("ha")
+    with open(path, 'wb') as f:
+        f.write(r.content)
+        f.close
